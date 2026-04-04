230caa473c3a9a3e0aba01557cc9bb467eea3dba
Logan Gardner <logang@southern.edu>
2026-04-04 08:50:30 -0400

    Add MUI sidebar layout and global CSS
    Introduce a collapsible sidebar AppLayout using MUI (IconButton, Box) and MUI icons; add @mui/icons-material to dependencies. Add global index.css with dark background and full-height root. Add Yarn config (nodeLinker: node-modules) and install state; update lockfile.

--- instaclone/src/Common/layouts/AppLayout.tsx
+++ instaclone/src/Common/layouts/AppLayout.tsx
@@ -1,7 +1,47 @@
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import FeedIcon from '@mui/icons-material/Feed';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { Outlet } from "react-router";
+import { useState } from "react";
+
+const IconButtonStyle = {
+    width: '40px'
+}
 
 export default function AppLayout() {
-    return <>
-        <Outlet />
-    </>
+    const [expanded, setExpanded] = useState(false);
+
+    return <Box sx={{
+        display: "flex",
+        height: "100%",
+    }}>
+            <Box sx={{
+                borderRight: "2px solid black",
+                display: "flex",
+                flexDirection: "column",
+                // alignItems: expanded ? "flex-start" : "center",
+                paddingY: "5px",
+                paddingX: "10px",
+                transition: "width 0.3s ease-in-out",
+                width: expanded ? "200px" : "50px",
+            }}>
+                <IconButton sx={IconButtonStyle} onClick={() => setExpanded(!expanded)}>
+                     <ArrowBackIosNewIcon sx={{
+                        rotate: expanded ? "" : "180deg"
+                     }}></ArrowBackIosNewIcon>
+                </IconButton>
+                <IconButton sx={IconButtonStyle}>
+                   <FeedIcon></FeedIcon>
+                </IconButton>
+                <IconButton sx={IconButtonStyle}>
+                   <FeedIcon></FeedIcon>
+                </IconButton>
+                <IconButton sx={IconButtonStyle}>
+                   <FeedIcon></FeedIcon>
+                </IconButton>
+            </Box>
+            <Outlet />
+    </Box>
+    
 }
